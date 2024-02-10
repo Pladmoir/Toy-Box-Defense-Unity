0b318291a1e8fe3267c8f4e7ded557e2dcf4efda
Tarunvir <trandhawa2007@gmail.com>
2024-02-10 17:52:09 -0500

    enemy movement complete

--- Toy Box Defense/Assets/LevelManager.cs
+++ Toy Box Defense/Assets/LevelManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public static LevelManager Main;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public Transform startPoint;
+        public Transform[] path;
+    
+        private void Awake()
+        {
+            Main = this; 
+         }
 }
