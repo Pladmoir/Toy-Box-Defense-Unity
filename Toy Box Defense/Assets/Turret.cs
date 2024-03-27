0cf6afd6bb18562e5af96c24dd3bfc5b926437d5
shivanbir <159091877+shivanbir@users.noreply.github.com>
2024-03-26 21:39:52 -0400

    Restarted the turret

--- Toy Box Defense/Assets/Turret.cs
+++ Toy Box Defense/Assets/Turret.cs
@@ -1,53 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-
+using UnityEditor;
 public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     [Header("References")]
-    [SerializeField] private LayerMask enemyMask; 
+    [SerializeField] private Transform turretRotationPoint;
+    
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
 
     private Transform target;
 
-    private void Update() {
+    private void Update()
+    {
         if (target == null)
         {
             FindTarget();
-            return;
         }
     }
-       
-        }
-   
-        
-    
-
-{
-    RaycastHit2D[] hits = Physics2D.CircleCastAll(Transform.position, targetingRange, (Vector2)
-    Transform.position, 0f, enemyMask);
-
-    if (hits.Length > 0)
-    {
-        Target = hits[0].transform;
-    }
-}
     private void OnDrawGizmosSelected()
     {
         Handles.color = Color.cyan;
